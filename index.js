0346b741539b3723f8e3e07704fdca0fd49b9caf
206lee <141048067+206lee@users.noreply.github.com>
2024-07-21 21:48:06 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(function(){
+    // 게시판 마우스 오버시 하부게시판 리스트 보이기
+    // if($(".sub-nav-list").is(':visible')){
+    //     $(".sub-nav-list").fadeOut(500);
+    // }
+    // else{
+    //     $(".sub-nav-list").fadeOut(500);
+    // };
+
+    // $(".nav-item").click(function(e){
+    //     e.preventDefault();
+    //     console.log("상위게시판 클릭");
+    // });
+});
